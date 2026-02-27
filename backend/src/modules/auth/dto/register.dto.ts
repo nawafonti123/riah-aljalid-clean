7622f0e09cb9e91af38c48f5352d0602f78ec155
nawafonti123 <132528240+nawafonti123@users.noreply.github.com>
2026-02-27 03:26:28 +0200

    Update register.dto.ts

--- backend/src/modules/auth/dto/register.dto.ts
+++ backend/src/modules/auth/dto/register.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsOptional()
+  @IsString()
   role?: string;
 }
